f4b2f1ef43b5b8fef14756a7cefdd06a7fbd1a9d
waterscorbin <101009966+waterscorbin@users.noreply.github.com>
2022-03-05 21:24:16 +0530

    Update script.py

--- plugins/script.py
+++ plugins/script.py
@@ -13,7 +13,7 @@ I can Mux Any srt or ass File in File or Video
 
 Use Help Command to Know How to Use me
 
-Made With 💕 By @Tellybots_4u
+Deployed By 💕 By ༺🅹🅾️🅷🅽 🅳🅾️🅴༻
 """
     HELP_TEXT = """
 Recommended
@@ -28,23 +28,17 @@ Softmux
 HardMux
 ➠ Send /hardmux to add Subtitle hardly in it 
 
-Made With 💕 By @Tellybots_4u
+Deployed By 💕 By ༺🅹🅾️🅷🅽 🅳🅾️🅴༻
 """
     ABOUT_TEXT = """
- **🤖 Bot :** Sub-Muxer\n
- **👲 Developer :** [Tellybots_4u](https://telegram.me/tellybots_4u)\n
- **👥 Channel :** [Tellybots_4u](https://telegram.me/tellybots_4u)\n
+ **🤖 Bot :** John Doe Subtitle Muxer\n
  **❄️ Credits :** Everyone in this journey\n
- **🍴 Source :** [Click here](https://t.me/tellybots_digital)\n
  **📝 Language :** [Python3](https://python.org)\n
  **📚 Library :** [Pyrogram v1.2.0](https://pyrogram.org)\n
  **🌟 Server :** [Heroku](https://heroku.com)\n
 """
     START_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('🤖 Update Channel', url='https://telegram.me/tellybots_4u'),
-        InlineKeyboardButton('💬 Support Group', url='https://telegram.me/tellybots_support')
-        ],[
         InlineKeyboardButton('❔ Help', callback_data='help'),
         InlineKeyboardButton('⛔ Close', callback_data='close')
         ]]
