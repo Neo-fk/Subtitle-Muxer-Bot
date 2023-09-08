18023db344757c278b4f8c9cc9b4dc8d62a928de
Anuwa <81525953+Neo-fk@users.noreply.github.com>
2023-09-08 15:50:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,21 +3,21 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    APP_ID = os.environ.get('APP_ID', None)
-    API_HASH = os.environ.get('API_HASH', None)
+    BOT_TOKEN = os.environ.get('6328374317:AAHkCzq47FK252GeJ_Uf1HNxRut24GPKpoI', None)
+    APP_ID = os.environ.get('22404062', None)
+    API_HASH = os.environ.get('162d4eb8c9cbfe558121c6cf41f8df43', None)
 
     #comma seperated user id of users who are allowed to use
 
     DOWNLOAD_DIR = 'downloads'
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1316963576))
+    OWNER_ID = int(os.environ.get("1165215979", 1316963576))
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)  
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("1165215979", ""))
     # database session name, example: xurluploader
-    SESSION_NAME = os.environ.get("SESSION_NAME", "")
+    SESSION_NAME = os.environ.get("Project 0", "")
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    DATABASE_URL = os.environ.get("mongodb+srv://Anuwa:Anuwa@cluster0.qsvet.mongodb.net/?retryWrites=true&w=majority", "")
+    LOG_CHANNEL = int(os.environ.get("dpx_log", "-100"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
     DOWNLOAD_LOCATION = "./DOWNLOADS"
